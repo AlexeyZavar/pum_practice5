0b4127d3ddb4172e6402be0000d5496474b4657e
AlexeyZavar <sltkval1@gmail.com>
2022-04-01 13:15:16 +0300

    Add integral

--- src/evaluator/solvers.py
+++ src/evaluator/solvers.py
@@ -5,6 +5,7 @@ from .wrapper import EasyWrapper
 logger = logging.getLogger('Lexer')
 
 EPSILON = 0.00001
+N = 100000
 
 
 def solve_using_secant(expr: EasyWrapper, a: float, b: float):
@@ -28,3 +29,16 @@ def solve_using_secant(expr: EasyWrapper, a: float, b: float):
             break
 
     return x
+
+
+def integral_using_simpson(expr: EasyWrapper, a: float, b: float):
+    # powered by Johny
+    # adopted by me <3
+    dx = (b - a) / N
+    res = 0
+
+    for i in range(N):
+        # https://en.wikipedia.org/wiki/Simpson%27s_rule
+        res += (dx / 6) * (expr(a + i * dx) + 4 * expr((2 * a + (2 * i + 1) * dx) / 2) + expr(a + (i + 1) * dx))
+
+    return res
